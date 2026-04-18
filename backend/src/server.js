6eda8a753497d17bf1a4af50d67c690d8d95c15e
suveshtglevel <suveshtglevel@gmail.com>
2026-04-18 10:13:12 +0530

    testing ci cd and rabbitcode on suvesh branch

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import app from './app.js';
 import connectDB from './config/db.js';
 
 const PORT = process.env.PORT || 5000;
-
+console.log(server)
 // Connect to Database
 connectDB().then(() => {
     pp.listen(PORT, () => {
