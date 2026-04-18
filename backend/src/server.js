c395ebd6e552e51fd742aad09e5f153bdfcf3aba
suveshtglevel <suveshtglevel@gmail.com>
2026-04-18 09:59:00 +0530

    testing ci cd and rabbitcode on suvesh branch

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,8 @@ import connectDB from './config/db.js';
 const PORT = process.env.PORT || 5000;
 
 // Connect to Database
-connectD().then(() => {
-    app.isten(PORT, () => {
+connectDB().then(() => {
+    pp.listen(PORT, () => {
         console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
         console.log(`Swagger documentation available at http://localhost:${PORT}/api-docs`);
     });
